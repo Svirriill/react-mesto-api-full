ffbfb1f4b525522a3a5121f579516d38d0d3f25e
Svirriill <sverelb@bk.ru>
2020-11-27 19:49:30 +0300

    react-mesto-api-full

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(cookieParser());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -39,13 +46,6 @@ app.use(helmet());
 
 app.use(requestLogger);
 
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
