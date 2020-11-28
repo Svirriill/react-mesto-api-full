60c0a975c2386036e593449a215797e7f6898a22
Svirriill <sverelb@bk.ru>
2020-11-28 10:13:33 +0300

    react-mesto-api-full

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cookieParser());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
